54e4c26b72dc8801c4dc3a9c827a69e1ee5eea34
Niembar <137523812+Niembar@users.noreply.github.com>
2023-10-31 13:58:47 +0100

    We found it, after hours and hours!!!!

--- helpers.R
+++ helpers.R
@@ -16,10 +16,11 @@ library(tmaptools)
 library(ggplot2)
 library(sf)
 library(shinylogs)
+library(Cairo)
 
 #Nik:
 #Laptop: nikla, UniPC: nbartlome, Zuhause: Niklaus Emanuel
-#setwd("C:/Users/nbartlome/OneDrive/1_Universit\u00E4t/4_PhD/10_R with R/Shiny R/ClimeApp_all/ClimeApp")
+setwd("C:/Users/nbartlome/OneDrive/1_Universit\u00E4t/4_PhD/10_R with R/Shiny R/ClimeApp_all/ClimeApp")
 
 #Richard:
 #Laptop/desktop:
