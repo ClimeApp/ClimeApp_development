ac65634d8ab6b8d292171f19a412a344de8125a0
Niembar <137523812+Niembar@users.noreply.github.com>
2024-02-29 17:03:20 +0100

    Z500 Correction

--- helpers.R
+++ helpers.R
@@ -170,7 +170,7 @@ dataset_variable_popover = function(popover_ID){
     "- Temperature - air temperature at 2m [°C]",br(),
     "- Precipitation - total monthly precipitation [mm]",br(),
     "- SLP - sea level pressure [hPa]",br(),
-    "- Z500 - pressure at 500 hPa geopotential height [m]",br(),br(),
+    "- Z500 - pressure at 500 hPa geopotential height [km]",br(),br(),
     "See",em("ModE data"),"tab on the Welcome page for more information.",
     id = popover_ID,
     placement = "right",
@@ -629,7 +629,7 @@ load_ModE_data = function(dataset,variable){
     } else if (variable == "SLP"){
       data_output = ncvar_get(data_nc,varid="slp")/100 
     } else {
-      data_output = ncvar_get(data_nc,varid="geopotential_height")/100
+      data_output = ncvar_get(data_nc,varid="geopotential_height")/1000
     }
     
     nc_close(data_nc)
@@ -653,7 +653,7 @@ load_ModE_data = function(dataset,variable){
     } else if (variable == "SLP"){
       data_output = ncvar_get(data_nc,varid="slp")/100 
     } else {
-      data_output = ncvar_get(data_nc,varid="geopoth")/100
+      data_output = ncvar_get(data_nc,varid="geopoth")/1000
     }
     
     # remove the first year (1421)
@@ -679,7 +679,7 @@ load_ModE_data = function(dataset,variable){
     } else if (variable == "SLP"){
       data_output = ncvar_get(data_nc,varid="slp")/100 
     } else {
-      data_output = ncvar_get(data_nc,varid="geopotential_height")/100
+      data_output = ncvar_get(data_nc,varid="geopotential_height")/1000
     }
     
     nc_close(data_nc)
@@ -866,7 +866,7 @@ generate_titles = function(tab,dataset,variable,mode,map_title_mode,ts_title_mod
   } else if (variable == "SLP"){
     v_unit = "[hPa]"
   } else if (variable == "Z500"){
-    v_unit = "[m]"
+    v_unit = "[km]"
   }  
   
   if (mode == "Absolute"){
@@ -1326,7 +1326,7 @@ rewrite_tstable = function(tstable,variable){
   } else if (variable == "Precipitation"){
     v_unit = "[mm]"
   } else if (variable == "SLP"|variable == "Z500"){
-    v_unit = "[m]"
+    v_unit = "[km]"
   } else {
     v_unit = ""
   }
@@ -2654,7 +2654,7 @@ generate_correlation_titles = function(variable1_source,variable2_source,
       V1_color = "purple4" ; V1_unit = "[hPa]"
     }
     else if (variable1 == "Z500"){
-      V1_color = "green4" ; V1_unit = "[m]"
+      V1_color = "green4" ; V1_unit = "[km]"
     }
     # Generate lon/lat addition
     if (variable1_lon_range[1]==variable1_lon_range[2]) {
@@ -2705,7 +2705,7 @@ generate_correlation_titles = function(variable1_source,variable2_source,
       V2_color = "purple4" ; V2_unit = "[hPa]"
     }
     else if (variable2 == "Z500"){
-      V2_color = "green4" ; V2_unit = "[m]"
+      V2_color = "green4" ; V2_unit = "[km]"
     }
     # Generate lon/lat addition
     if (variable2_lon_range[1]==variable2_lon_range[2]) {
@@ -3234,7 +3234,7 @@ generate_regression_titles = function(independent_source,dependent_source,
     } else if (modERA_dependent_variable == "SLP"){
       color_d = "purple4" ; unit_d = "[hPa]"
     } else if (modERA_dependent_variable == "Z500"){
-      color_d = "green4" ; unit_d = "[m]"
+      color_d = "green4" ; unit_d = "[km]"
     }
   }
   
